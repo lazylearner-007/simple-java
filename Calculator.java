d234c2326cb8dfb03afddaa818335c74de16c1fc
lazylearner-007 <76438087+lazylearner-007@users.noreply.github.com>
2020-12-21 19:23:59 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+ initimport java.util.*;
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter Numbers :");
+        Scanner inp= new Scanner(System.in);
+        int number1,number2;
+        number1 = inp.nextInt();
+        number2 = inp.nextInt();
+
+        System.out.println("Enter 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
+        int choose;
+        choose = inp.nextInt();
+
+        switch (choose)
+        {
+            case 1 -> System.out.println(add(number1, number2));
+            case 2 -> System.out.println(sub(number1, number2));
+            case 3 -> System.out.println(mult(number1, number2));
+            case 4 -> System.out.println(div(number1, number2));
+        }
+
+
+    }
+    public static int add(int x, int y)
+    {
+        return x + y;
+    }
+    public static int sub(int x, int y)
+    {
+        return x-y;
+    }
+    public static int mult(int x, int y)
+    {
+        return x*y;
+    }
+    public static int div(int x, int y)
+    {
+        return x/y;
+    }
+
+}
